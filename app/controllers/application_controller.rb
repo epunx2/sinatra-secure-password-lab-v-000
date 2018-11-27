0ef797d6f9a19344907f82d2454b02f65795c5b0
Eric Chrobak <eric@ericchrobak.com>
2018-11-27 06:51:48 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     #your code here
     # binding.pry
-    if params[:username] == nil && params[:username] == "" && params[:password] == nil && params[:password] == ""
+    if params[:username] == nil || params[:username] == "" || params[:password] == nil || params[:password] == ""
       redirect "/failure"
     else
       user = User.new(:username => params[:username], :password => params[:password])
